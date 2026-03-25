2b253e7b7bded6979ef182095866175bfea7d72d
tali-creator <talinanzing111@gmail.com>
2026-03-25 14:19:50 +0100

    feat: Add logging bounds

--- contracts/crowdfund/src/lib.rs
+++ contracts/crowdfund/src/lib.rs
@@ -30,16 +30,15 @@ mod auth_tests;
 pub mod campaign_goal_minimum;
 #[cfg(test)]
 mod campaign_goal_minimum_test;
-pub mod contract_state_size;
-#[cfg(test)]
-#[path = "contract_state_size.test.rs"]
-mod contract_state_size_test;
 pub mod contribute_error_handling;
 #[cfg(test)]
 mod contribute_error_handling_tests;
 pub mod proptest_generator_boundary;
 #[cfg(test)]
 mod proptest_generator_boundary_tests;
+pub mod stellar_token_minter;
+#[cfg(test)]
+mod stellar_token_minter_test;
 #[cfg(test)]
 mod refund_single_token_tests;
 #[cfg(test)]
